20f342a49696e1813095270c8522c7411bb858cf
Leandro Plaza <leandroplazaestudos10@gmail.com>
2026-03-23 09:28:36 -0300

    Projeto calculadora com testes pytest e GitHub Actions

---
+++ tests/test_operacoes.py
@@ -0,0 +1,17 @@
+from calculadora.operacoes import soma, subtracao, multiplicacao, divisao
+
+
+def test_soma():
+    assert soma(2, 3) == 5
+
+
+def test_subtracao():
+    assert subtracao(5, 3) == 2
+
+
+def test_multiplicacao():
+    assert multiplicacao(2, 4) == 8
+
+
+def test_divisao():
+    assert divisao(10, 2) == 5
